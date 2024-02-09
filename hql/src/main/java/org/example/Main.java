b486c0e8c32dad3978c0e6a95538da8ca1d9edb0
MinuraWije <minurawije@gmail.com>
2024-02-09 10:37:00 +0530

    hql module done.

--- hql/src/main/java/org/example/Main.java
+++ hql/src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import org.example.config.FactoryConfiguration;
+import org.example.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
 
+        Query query = session.createQuery("from Student");
+        List<Student> studentList = query.list();
+        for(Student student:studentList){
+            System.out.println(student.getName());
+        }
+        transaction.commit();
+        session.close();
     }
 }
