1fd0e2c2db09b2e1e1ceea82d88375b25da364e2
MinuraWije <minurawije@gmail.com>
2024-02-09 10:28:58 +0530

    added main files of sql module.

---
+++ sql/src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.example.config.FactoryConfiguration;
+import org.example.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.NativeQuery;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        NativeQuery nativeQuery = session.createNativeQuery("SELECT * FROM STUDENT");
+        nativeQuery.addEntity(Student.class);
+
+        List<Student> studentList = nativeQuery.list();
+        for(Student s:studentList){
+            System.out.println(s.getName());
+        }
+
+        transaction.commit();
+        session.close();
+    }
+}
